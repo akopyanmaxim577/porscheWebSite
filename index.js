82f6b4bcd13bfada64df8970a9645c5b006bdecb
Maxim Akopyan <akopyanmaxim57@gmail.com>
2022-11-29 23:37:40 +0200

    Responsive swiper

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 5,
-    spaceBetween: 6,
+    spaceBetween: 300,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
