66305c96e714ad18efa8bf4cb4fc0047d1b4530d
Maxim Akopyan <akopyanmaxim57@gmail.com>
2022-12-01 14:56:10 +0200

    add slider

--- index.js
+++ index.js
@@ -6,3 +6,27 @@
       clickable: true,
     },
   }); */
+
+const add = document.getElementById("add");
+const main = document.getElementById("main");
+const close = document.getElementById('close');
+
+const link= document.querySelectorAll('#link')
+
+add.addEventListener('click',function(){
+  main.classList.add('db')
+})
+
+close.addEventListener('click',function(){
+  main.classList.remove('db')
+})
+
+link.forEach(n=>n.addEventListener('click',function(){
+  main.classList.remove('db')
+}))
+
+
+
+
+
+  
