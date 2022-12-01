b24c7105f7e8aa92edd81aeaf3e908aefea2167d
Maxim Akopyan <akopyanmaxim57@gmail.com>
2022-12-01 20:53:41 +0200

    Modal

--- index.js
+++ index.js
@@ -64,6 +64,28 @@ link.forEach(n=>n.addEventListener('click',function(){
 
 
 
+/* World phone */
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
+
+/* Modal */
+const m_open = document.getElementById('banner_open');
+const m_close = document.getElementById('m_close');
+const modal = document.getElementById('mod')
+
+
+m_open.addEventListener('click',function(){
+  modal.classList.add('deb')
+})
+
+
+m_close.addEventListener('click',function(){
+  modal.classList.remove('deb')
+})
 
 
   
