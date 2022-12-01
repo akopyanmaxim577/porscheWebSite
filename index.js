15d6b7be1afc5584879d121cb3c916b92d587898
Maxim Akopyan <akopyanmaxim57@gmail.com>
2022-12-01 16:43:42 +0200

    Add and fix swiper

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 5,
-    spaceBetween: 300,
+    spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    loop:true,
+    breakpoints:{
+      250:{
+        slidesPerView: 1,
+        
+
+      },
+      354:{
+        slidesPerView: 1.5,
+        spaceBetween: 100,
+
+      },
+
+      562:{
+        slidesPerView: 2,
+        spaceBetween: 50,
+
+      },
+      655:{
+        slidesPerView: 3,
+        spaceBetween: 250,
+
+      },
+      913:{
+        slidesPerView: 4,
+        spaceBetween: 300,
+
+      },
+      1035:{
+        slidesPerView: 5,
+        spaceBetween: 300,
+
+      }
+
+      
+
+    }
   });
 
 const add = document.getElementById("add");
